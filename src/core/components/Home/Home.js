e3f63e7a7d60f77e003a8a165791b27e7d9e277e
juanjou <jouglardjuan@gmail.com>
2019-11-13 15:20:15 -0300

    Markers in custom map

--- src/core/components/Home/Home.js
+++ src/core/components/Home/Home.js
@@ -1,15 +1,9 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
-import firestore from '@react-native-firebase/firestore';
 
 export default class Home extends Component {
     
-    async componentDidMount(){
-        const querySnapshot = await firestore()
-                                    .collection('Locales')
-                                    .get();
-        console.log('Docs',querySnapshot.docs);
-    }
+    
     
     render() {
         return (<Text>Home works!</Text>)
--- src/shared/components/objectsOnMap/objectsOnMap.js
+++ src/shared/components/objectsOnMap/objectsOnMap.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Marker } from 'react-native-maps';
 import { MarkersFactory } from '../../../core/factories/markersFactories';
+import firestore from '@react-native-firebase/firestore';
 
 export default class ObjectsOnMap extends Component {
     
@@ -9,7 +10,20 @@ export default class ObjectsOnMap extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            places: []
+        }
     }
+
+    componentDidMount() {
+		firestore()
+		.collection('Places')
+			.get().then(
+				(documents) => {
+                    this.setState({places: documents.docs});
+				}
+			);
+   }
     
 	clickMarker(e) {
         console.log(e.nativeEvent);
@@ -17,12 +31,25 @@ export default class ObjectsOnMap extends Component {
     }
 
     render() {
-        return (
-                <Marker
-                    {...this.markersFactory.markersForMap()}
-                    onPress={this.clickMarker}>
-                </Marker>
-        );
+        let places = [];
+        if (this.state.places !== []) {
+            places = this.state.places.map(
+                (place) => {
+                    console.log('Place',place.data().location);
+                    return (<Marker
+                                onPress={this.clickMarker}
+                                key={place.data().name}
+                                coordinate= {{
+                                    latitude: place.data().location.latitude,
+                                    longitude: place.data().location.longitude}}
+                                description= {place.data().type}
+                                title={place.data().name}>
+                            </Marker>);
+                }
+            );
+        }
+        
+        return places;
     }
 }
 
