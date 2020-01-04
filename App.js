9c7321d7c22922a713a94045cdd3531e20101126
juanjou <jouglardjuan@gmail.com>
2020-01-04 00:45:17 -0300

    Authentication flow

--- App.js
+++ App.js
@@ -1,13 +1,56 @@
-import Map from './src/core/components/Map/Map';
-import Home from './src/core/components/Home/Home';
-import Profile from './src/core/components/Profile/Profile';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
-import { createAppContainer } from 'react-navigation';
+import React, { Component } from 'react';
+import { GoogleSignin } from 'react-native-google-signin';
+import { firebase } from '@react-native-firebase/auth';
+import Main from './src/core/components/Main/Main';
+export default class App extends Component {
 
-const tabNavigator = createBottomTabNavigator({
-  Home: Home,
-  Map: Map,
-  Profile: Profile
-});
+  constructor() {
+    super();
+    this.state = {
+      isLoggedIn: false
+    };
+  }
 
-export default createAppContainer(tabNavigator);
+
+  componentDidMount() {
+    this.googleLogin();
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user)
+        this.setState({isLoggedIn: true});
+    });
+  }
+
+  async componentWillUnmount() {
+    await firebase.auth().signOut();
+    await GoogleSignin.signOut();
+  }
+
+
+  async googleLogin() {
+    try {
+      GoogleSignin.configure({
+        offlineAccess: true,
+        webClientId: '710418825047-olbh8tqc7vb3fkdv8bkso7sdu3npkvpe.apps.googleusercontent.com'
+      });
+  
+      const userData = await GoogleSignin.signIn();
+      const credential = firebase.auth.
+      GoogleAuthProvider.credential(userData.idToken, userData.accessToken);
+      console.log('Credential',credential);
+      const user=await firebase.auth().signInWithCredential(credential);
+      console.log('isNewUser',user);
+    } catch (e) {
+      console.log('error');
+      console.error(e);
+    }
+  }
+
+  render() {
+    if (this.state.isLoggedIn)
+      return <Main></Main>;
+    else
+      return null;
+  }
+} 
+
+/**/
