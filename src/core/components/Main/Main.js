c2603cc0419d9c46eabb0f07bd7b9681c97aaed8
juanjou <jouglardjuan@gmail.com>
2020-01-05 23:00:50 -0300

    Icons for tabs

--- src/core/components/Main/Main.js
+++ src/core/components/Main/Main.js
@@ -5,6 +5,9 @@ import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { createStackNavigator } from 'react-navigation-stack'; 
 import { createAppContainer } from 'react-navigation';
 import LocalDetail from '../../../shared/components/localDetail/localDetail';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faHome, faMap, faUser } from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
 
 const HomeNavigator = createStackNavigator({
     Home,
@@ -17,9 +20,53 @@ const HomeNavigator = createStackNavigator({
   });
 
 const tabNavigator = createBottomTabNavigator({
-    Home: HomeNavigator,
-    Map: Map,
-    Profile: Profile
+    Home: {
+      navigationOptions: {
+        // eslint-disable-next-line react/display-name
+      tabBarIcon: ({focused}) => { 
+        if (focused) {
+          return <FontAwesomeIcon color='#464646' icon={faHome} size={30}/>;
+        }
+        else {
+          return <FontAwesomeIcon color='#C6C6C5' icon={faHome} size={30}/>;
+        }
+      }
+      },
+      screen: HomeNavigator,
+    },
+    Map: {
+      navigationOptions: {
+        // eslint-disable-next-line react/display-name
+        tabBarIcon: ({focused}) => { 
+          if (focused) {
+            return <FontAwesomeIcon color='#464646' icon={faMap} size={30}/>;
+          }
+          else {
+            return <FontAwesomeIcon color='#C6C6C5' icon={faMap} size={30}/>;
+          }
+        }
+      },
+      screen: Map
+    },
+    Profile: {
+      navigationOptions: {
+        // eslint-disable-next-line react/display-name
+        tabBarIcon: ({focused}) => { 
+          if (focused) {
+            return <FontAwesomeIcon color='#464646' icon={faUser} size={30}/>;
+          }
+          else {
+            return <FontAwesomeIcon color='#C6C6C5' icon={faUser} size={30}/>;
+          }
+        }
+      },
+      screen: Profile
+    }
+  },
+  {
+    tabBarOptions: {
+      showLabel: false
+    }
   });
   
 export default createAppContainer(tabNavigator); 
