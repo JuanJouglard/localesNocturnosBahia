4adc910da8c2c3cd92f21e5741892f0e997a366a
juanjou <jouglardjuan@gmail.com>
2020-01-10 16:41:22 -0300

    Prettier changes

--- src/core/components/Map/Map.js
+++ src/core/components/Map/Map.js
@@ -1,75 +1,75 @@
-
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import  MapView from 'react-native-maps';
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import MapView from 'react-native-maps';
 import Geolocation from 'react-native-geolocation-service';
 import ObjectsOnMap from '../../../shared/components/objectsOnMap/objectsOnMap';
-import { customStyle } from '../../configurations/map/customStyle';
+import {customStyle} from '../../configurations/map/customStyle';
+import {PropTypes} from 'prop-types';
 
 export default class Map extends Component {
+  mapConfiguration = {
+    mapType: 'standard',
+    showsTraffic: true,
+    showsUserLocation: true,
+  };
 
-    mapConfiguration = {
-		mapType: 'standard',
-		showsTraffic: true,
-        showsUserLocation: true,
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: null,
+      latitudeDelta: 0.0922,
+      longitude: null,
+      longitudeDelta: 0.0421,
+      selectedMarker: 'Hola',
     };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            latitude: null,
-            latitudeDelta: 0.0922,
-            longitude: null,
-			longitudeDelta: 0.0421,
-			selectedMarker: 'Hola'
-        }
-	}
+  selectMarker = marker => {
+    console.log(marker);
+    this.props.navigation.navigate('Local', {
+      item: marker,
+    });
+  };
 
-	
-
-	selectMarker = (marker) => {
-		console.log(this.state);
-	}
-    
-    render() {
-        Geolocation.watchPosition(
-            (position) => {
-                this.setState({...position.coords});
-            }
-        );
-        return this.state.longitude !== null ? (
-			<View style={styles.mapContainer}>
-				<MapView
-					style={styles.mapView}
-					{...this.mapConfiguration}
-					region = {this.state}
-					customMapStyle= {customStyle}
-				>
-					<ObjectsOnMap onMarkerSelect={this.selectMarker}
-							zoomLevel={
-								Math.round(
-								Math.
-									log(360/this.state.longitudeDelta)/Math.LN2)
-							}>
-					</ObjectsOnMap>
-				</MapView>
-				<View style={styles.locationInformation}>
-					<Text>{this.state.selectedMarker}</Text>
-				</View>
-			</View>
-        ): <Text>No  Coords</Text>;
-    }
+  render() {
+    Geolocation.watchPosition(position => {
+      this.setState({...position.coords});
+    });
+    return this.state.longitude !== null ? (
+      <View style={styles.mapContainer}>
+        <MapView
+          style={styles.mapView}
+          {...this.mapConfiguration}
+          region={this.state}
+          customMapStyle={customStyle}>
+          <ObjectsOnMap
+            onMarkerSelect={this.selectMarker}
+            zoomLevel={Math.round(
+              Math.log(360 / this.state.longitudeDelta) / Math.LN2,
+            )}></ObjectsOnMap>
+        </MapView>
+        <View style={styles.locationInformation}>
+          <Text>{this.state.selectedMarker}</Text>
+        </View>
+      </View>
+    ) : (
+      <Text>No Coords</Text>
+    );
+  }
 }
 
+Map.propTypes = {
+  navigation: PropTypes.object.isRequired,
+};
+
 const styles = StyleSheet.create({
-	locationInformation: {
-		flex: 2
-	},
-	mapContainer: {
-		flex: 1,
-	},
-    mapView: {
-		flex: 4,
-    }
+  locationInformation: {
+    flex: 2,
+  },
+  mapContainer: {
+    flex: 1,
+  },
+  mapView: {
+    flex: 4,
+  },
 });
-    
