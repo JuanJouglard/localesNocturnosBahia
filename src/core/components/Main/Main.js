1f8f09884c62481b0220301270d80fe7d618911c
juanjou <jouglardjuan@gmail.com>
2020-01-10 16:41:09 -0300

    Refactor of Home screen

--- src/core/components/Main/Main.js
+++ src/core/components/Main/Main.js
@@ -1,78 +1,65 @@
 import Map from '../Map/Map';
 import Home from '../Home/Home';
 import Profile from '../Profile/Profile';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
-import { createStackNavigator } from 'react-navigation-stack'; 
-import { createAppContainer } from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import {createStackNavigator} from 'react-navigation-stack';
+import {createAppContainer} from 'react-navigation';
 import LocalDetail from '../../../shared/components/localDetail/localDetail';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faHome, faMapPin, faUser } from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faHome, faMapPin, faUser} from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 
-const tabNavigator = createBottomTabNavigator({
-  Home: {
-    navigationOptions: {
-      // eslint-disable-next-line react/display-name
-    tabBarIcon: ({focused}) => { 
-      if (focused) {
-        return <FontAwesomeIcon color='#464646' icon={faHome} size={30}/>;
-      }
-      else {
-        return <FontAwesomeIcon color='#C6C6C5' icon={faHome} size={30}/>;
-      }
-    }
-    },
-    screen: Home,
-  },
-  Map: {
-    navigationOptions: {
-      // eslint-disable-next-line react/display-name
-      tabBarIcon: ({focused}) => { 
-        if (focused) {
-          return <FontAwesomeIcon color='#464646' icon={faMapPin} size={30}/>;
-        }
-        else {
-          return <FontAwesomeIcon color='#C6C6C5' icon={faMapPin} size={30}/>;
-        }
-      }
-    },
-    screen: Map
-  },
-  Profile: {
-    navigationOptions: {
-      // eslint-disable-next-line react/display-name
-      tabBarIcon: ({focused}) => { 
-        if (focused) {
-          return <FontAwesomeIcon color='#464646' icon={faUser} size={30}/>;
-        }
-        else {
-          return <FontAwesomeIcon color='#C6C6C5' icon={faUser} size={30}/>;
-        }
-      }
-    },
-    screen: Profile
+const iconForTab = icon => ({focused}) => {
+  if (focused) {
+    return <FontAwesomeIcon color="#464646" icon={icon} size={30} />;
+  } else {
+    return <FontAwesomeIcon color="#C6C6C5" icon={icon} size={30} />;
   }
-},
-{
-  tabBarOptions: {
-    showLabel: false
-  }
-});
+};
 
-const HomeNavigator = createStackNavigator({
-    Local: {
-      screen: LocalDetail
+const tabNavigator = createBottomTabNavigator(
+  {
+    Home: {
+      navigationOptions: {
+        tabBarIcon: iconForTab(faHome),
+      },
+      screen: Home,
     },
-    Main: {
-      screen: tabNavigator
-    }
+    Map: {
+      navigationOptions: {
+        tabBarIcon: iconForTab(faMapPin),
+      },
+      screen: Map,
+    },
+    Profile: {
+      navigationOptions: {
+        tabBarIcon: iconForTab(faUser),
+      },
+      screen: Profile,
+    },
   },
   {
-    initialRouteName: 'Main'
-  });
+    tabBarOptions: {
+      showLabel: false,
+    },
+  },
+);
 
- 
-  
+const HomeNavigator = createStackNavigator(
+  {
+    Local: {
+      screen: LocalDetail,
+    },
+    Main: {
+      navigationOptions: {
+        headerShown: false,
+      },
+      screen: tabNavigator,
+    },
+  },
+  {
+    initialRouteName: 'Main',
+  },
+);
 
-  
-export default createAppContainer(HomeNavigator); 
+export default createAppContainer(HomeNavigator);
