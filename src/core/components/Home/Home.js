dacf3b0bd7803735b3e20dbe50128f0f39b8f9ad
juanjou <jouglardjuan@gmail.com>
2020-01-21 17:16:27 -0300

    Tab view in Home

--- src/core/components/Home/Home.js
+++ src/core/components/Home/Home.js
@@ -1,26 +1,45 @@
 import React, {Component} from 'react';
-import {PlacesService} from '../../services.js/places';
+import {PlacesService} from '../../services/places';
 import PropTypes from 'prop-types';
 import ListOfEntries from '../../../shared/components/listOfEntries/listOfEntries';
-
+import {TabView, TabBar} from 'react-native-tab-view';
+import {Dimensions, StyleSheet} from 'react-native';
 export default class Home extends Component {
   placesService;
 
   constructor(props) {
     super(props);
     this.state = {
+      events: [],
+      index: 0,
       places: [],
+      routes: [
+        {
+          key: 'places',
+          title: 'Locales',
+        },
+        {
+          key: 'events',
+          title: 'Eventos',
+        },
+      ],
     };
   }
 
-  componentDidMount() {
-    this.placesService = PlacesService.getInstance();
-    this.placesService.getPlaces().then(places => {
-      this.setState({places: places.docs});
-    });
-  }
+  placesRoute = () => {
+    return (
+      <ListOfEntries
+        style={style.list}
+        onEntryPress={item =>
+          this.props.navigation.navigate('Local', {
+            item: item,
+          })
+        }
+        list={this.state.places}></ListOfEntries>
+    );
+  };
 
-  render() {
+  eventsRoute = () => {
     return (
       <ListOfEntries
         onEntryPress={item =>
@@ -28,13 +47,55 @@ export default class Home extends Component {
             item: item,
           })
         }
-        list={this.state.places.map(this.extractData)}></ListOfEntries>
+        list={this.state.events}></ListOfEntries>
     );
+  };
+
+  renderScene = ({route}) => {
+    switch (route.key) {
+      case 'places':
+        return this.placesRoute();
+      case 'events':
+        return this.eventsRoute();
+      default:
+        return null;
+    }
+  };
+
+  renderTabBar = props => <TabBar {...props} style={style.tabs}></TabBar>;
+
+  componentDidMount() {
+    this.placesService = PlacesService.getInstance();
+    this.placesService.getPlaces().then(places => {
+      this.setState({places: places});
+    });
+    this.placesService.getEvents().then(events => {
+      this.setState({events: events});
+    });
   }
 
-  extractData = item => item.data();
+  render() {
+    return (
+      <TabView
+        navigationState={{index: this.state.index, routes: this.state.routes}}
+        renderScene={this.renderScene}
+        renderTabBar={this.renderTabBar}
+        onIndexChange={index => this.setState({index: index})}
+        initialLayout={{width: Dimensions.get('window').width}}></TabView>
+    );
+  }
 }
 
 Home.propTypes = {
   navigation: PropTypes.object.isRequired,
 };
+
+const style = StyleSheet.create({
+  list: {
+    height: '100%',
+    width: '100%',
+  },
+  tabs: {
+    backgroundColor: '#656565',
+  },
+});
