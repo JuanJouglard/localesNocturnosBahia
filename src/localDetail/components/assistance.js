799945cae88106c473a4db78ba948b2dba63b0af
juanjou <jouglardjuan@gmail.com>
2020-01-28 19:53:10 -0300

    Local detail implementation

--- src/localDetail/components/assistance.js
+++ src/localDetail/components/assistance.js
@@ -1,43 +1,90 @@
 import React, {Component} from 'react';
-import {View, Text, TimePickerAndroid, StyleSheet} from 'react-native';
-import {faClock} from '@fortawesome/free-solid-svg-icons';
-import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
-import {TouchableOpacity} from 'react-native-gesture-handler';
-import {PropTypes} from 'prop-types';
+import {Button, View, Text, TimePickerAndroid, StyleSheet} from 'react-native';
+import DateService from '../services/date';
+import TimeStamp from './Time';
+import {Clock} from '../../shared/components/clock/Clock';
 
 export default class Assistance extends Component {
+  dateService;
+
+  constructor(props) {
+    super(props);
+    this.dateService = DateService.getInstance();
+    this.state = {
+      endTime: null,
+      startTime: null,
+    };
+  }
+
   render() {
     return (
       <View style={style.assistance}>
         <Text style={style.titleText}>¿Cuando vas a asistir?</Text>
         <View style={style.timePicker}>
-          <Clock onPress={this.openTimePicker}>
-            <Text>Hora de Inicio</Text>
-          </Clock>
-          <Clock onPress={this.openTimePicker}>
-            <Text>Hora de Fin</Text>
-          </Clock>
+          <View style={style.timeStamps}>
+            <Clock onPress={this.openTimePicker('startTime')}>
+              <Text>Desde</Text>
+            </Clock>
+            <TimeStamp
+              titleText="Desde"
+              date={this.getFriendlyDate(this.state.startTime)}
+              time={this.getFriendlyTime(this.state.startTime)}></TimeStamp>
+          </View>
+          <View style={style.timeStamps}>
+            <Clock
+              onPress={this.openTimePicker('endTime')}
+              disabled={this.state.startTime === null}>
+              <Text>Hasta</Text>
+            </Clock>
+            <TimeStamp
+              titleText="Hasta"
+              date={this.getFriendlyDate(this.state.endTime)}
+              time={this.getFriendlyTime(this.state.endTime)}></TimeStamp>
+          </View>
         </View>
+        <Button title="Registrar" disabled></Button>
       </View>
     );
   }
 
-  openTimePicker() {
-    TimePickerAndroid.open({
-      hour: new Date().getHours(),
-      is24Hour: true,
-      minute: new Date().getMinutes(),
-    });
+  getFriendlyDate(date) {
+    if (date) {
+      const dateArray = date.match(/(\d*)-(\d*)-(\d*)T(\d*):(\d*).*Z/);
+      const onlyDate = [dateArray[3], dateArray[2], dateArray[1]];
+      return dateArray ? onlyDate.join('/') : null;
+    } else return null;
   }
+
+  getFriendlyTime(date) {
+    if (date) {
+      const dateArray = date.match(/(\d*)-(\d*)-(\d*)T(\d*):(\d*).*Z/);
+      let fixedHour = dateArray[4] - 3;
+      if (fixedHour < 0) fixedHour = 24 + fixedHour;
+      if (fixedHour < 10) fixedHour = '0' + fixedHour;
+      const onlyTime = [fixedHour, dateArray[5]];
+      return dateArray ? onlyTime.join(':') : null;
+    } else return null;
+  }
+
+  openTimePicker = openClock => time => {
+    if (time.action !== TimePickerAndroid.dismissedAction)
+      this.setState(previousState => {
+        return this.dateService.validateTime(
+          openClock,
+          previousState.startTime,
+          time,
+        );
+      });
+  };
 }
 
 const style = StyleSheet.create({
-  assistance: {
-    flex: 3,
-  },
   timePicker: {
     flexDirection: 'row',
     justifyContent: 'space-evenly',
+  },
+  timeStamps: {
+    alignItems: 'center',
   },
   titleText: {
     fontFamily: 'Roboto-Light',
@@ -46,23 +93,3 @@ const style = StyleSheet.create({
     textAlign: 'center',
   },
 });
-
-function Clock(props) {
-  const style = StyleSheet.create({
-    clock: {
-      alignItems: 'center',
-    },
-  });
-
-  return (
-    <TouchableOpacity onPress={props.onPress} style={style.clock}>
-      <Text>{props.children}</Text>
-      <FontAwesomeIcon icon={faClock} size={32}></FontAwesomeIcon>
-    </TouchableOpacity>
-  );
-}
-
-Clock.propTypes = {
-  children: PropTypes.object,
-  onPress: PropTypes.func,
-};
