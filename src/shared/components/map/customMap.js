d039116c01442459119dedc7521ac227770f3726
juanjou <jouglardjuan@gmail.com>
2020-02-09 14:06:52 -0300

    Added factory for markers

--- src/shared/components/map/customMap.js
+++ src/shared/components/map/customMap.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
-import {Image} from 'react-native';
+import {Image, Text} from 'react-native';
 import * as images from '../../../core/images/images';
 import MapView, {Marker} from 'react-native-maps';
 import PropTypes from 'prop-types';
 import {customStyle} from '../../../core/configurations/map/customStyle';
+import {MarkersFactory} from '../../services/markersFactory';
 export default class CustomMap extends Component {
   mapConfiguration = {
     mapType: 'standard',
@@ -11,8 +12,11 @@ export default class CustomMap extends Component {
     showsUserLocation: true,
   };
 
+  markersFactory;
+
   constructor(props) {
     super(props);
+    this.markersFactory = MarkersFactory.getInstance();
   }
 
   render() {
@@ -33,25 +37,9 @@ export default class CustomMap extends Component {
 
   getMarkers() {
     return this.props.markers.map(marker => {
-      return (
-        <Marker
-          identifier={marker.id.toString()}
-          onPress={this.clickMarker(marker)}
-          key={marker.name}
-          coordinate={{
-            latitude: marker.location.latitude,
-            longitude: marker.location.longitude,
-          }}
-          description={marker.friendlyType}
-          title={marker.name}>
-          <Image
-            source={images[marker.type + 'IMAGE']}
-            style={{
-              height: 32,
-              width: 32,
-            }}></Image>
-        </Marker>
-      );
+      return this.markersFactory
+        .createMarker({clickMarker: this.clickMarker(marker), ...marker})
+        .render();
     });
   }
 }
