5e47e6b9db41e019fa8d2f828c892d421bc4e14f
juanjou <jouglardjuan@gmail.com>
2020-02-11 14:53:16 -0300

    Added animated navigation Keyboard mode Searchinput in map tab

--- src/core/components/Map/Map.js
+++ src/core/components/Map/Map.js
@@ -1,10 +1,12 @@
 //@ts-check
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Keyboard, StyleSheet, Text, View} from 'react-native';
 import Geolocation from 'react-native-geolocation-service';
 import PropTypes from 'prop-types';
 import CustomMap from '../../../shared/components/map/customMap';
 import {PlacesService} from '../../../shared/services/places';
+import SearchInput from '../../../shared/components/searchInput/searchInput';
+import ListOfEntries from '../../../shared/components/listOfEntries/listOfEntries';
 
 export default class Map extends Component {
   mapConfiguration = {
@@ -13,16 +15,22 @@ export default class Map extends Component {
     showsUserLocation: true,
   };
   placesService;
+  mapRef;
 
   constructor(props) {
     super(props);
     this.state = {
+      delta: {
+        latitudeDelta: 0.09,
+        longitudeDelta: 0.09,
+      },
+      initialMarkers: [],
       markers: [],
       region: {
         latitude: null,
-
         longitude: null,
       },
+      searchText: '',
     };
   }
 
@@ -30,6 +38,7 @@ export default class Map extends Component {
     this.placesService = PlacesService.getInstance();
     this.placesService.getPlaces().then(documents => {
       this.setState({markers: documents});
+      this.setState({initialMarkers: documents});
     });
   }
 
@@ -39,24 +48,60 @@ export default class Map extends Component {
     });
   };
 
+  filterPlaces = text => {
+    this.setState({
+      places: this.state.initialMarkers.filter(item =>
+        item.name.toUpperCase().startsWith(text.toUpperCase()),
+      ),
+      searchText: text,
+    });
+  };
+
+  navigateToMarker = selectedItem => {
+    Keyboard.dismiss();
+    this.mapRef.animateToRegion(
+      {
+        latitude: selectedItem.location._latitude,
+        latitudeDelta: 0.005,
+        longitude: selectedItem.location._longitude,
+        longitudeDelta: 0.005,
+      },
+      4000,
+    );
+    this.setState({
+      searchText: '',
+    });
+  };
+
   render() {
     Geolocation.watchPosition(position => {
       this.setState({region: position.coords});
     });
     return this.state.region.longitude !== null ? (
       <View style={styles.mapContainer}>
-        <CustomMap
-          style={styles.mapView}
-          layout={{flex: 4}}
-          region={{
-            latitude: this.state.region.latitude,
-            latitudeDelta: 0.0922,
-            longitude: this.state.region.longitude,
-
-            longitudeDelta: 0.0421,
-          }}
-          markers={this.state.markers}
-          onMarkerSelect={this.selectMarker}></CustomMap>
+        <SearchInput
+          section="Locales"
+          onInput={this.filterPlaces}></SearchInput>
+        {(() => {
+          if (this.state.searchText.length > 1)
+            return (
+              <View>
+                <ListOfEntries
+                  onEntryPress={this.navigateToMarker}
+                  list={this.state.places}></ListOfEntries>
+              </View>
+            );
+        })()}
+        <View style={styles.mapView}>
+          <CustomMap
+            mapRef={map => (this.mapRef = map)}
+            region={{
+              ...this.state.region,
+              ...this.state.delta,
+            }}
+            markers={this.state.markers}
+            onMarkerSelect={this.selectMarker}></CustomMap>
+        </View>
       </View>
     ) : (
       <Text>No Coords</Text>
@@ -77,5 +122,6 @@ const styles = StyleSheet.create({
   },
   mapView: {
     flex: 4,
+    zIndex: 0,
   },
 });
