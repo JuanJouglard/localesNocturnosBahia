dd2c7b8f7a9bf23395b2c3c0e685b5e5cbcfff8a
juanjou <jouglardjuan@gmail.com>
2020-02-12 22:32:44 -0300

    Added logOut functionality

--- src/shared/services/user.js
+++ src/shared/services/user.js
@@ -1,6 +1,6 @@
 import {firebase} from '@react-native-firebase/auth';
 import Geolocation from 'react-native-geolocation-service';
-
+import {GoogleSignin} from 'react-native-google-signin';
 export default class UserService {
   static instance;
 
@@ -17,5 +17,29 @@ export default class UserService {
 
   getUserLocation(callback) {
     return Geolocation.getCurrentPosition(callback);
+  }
+
+  logOut() {
+    return Promise.all([GoogleSignin.signOut(), firebase.auth().signOut()]);
+  }
+
+  async logIn() {
+    try {
+      GoogleSignin.configure({
+        offlineAccess: true,
+        webClientId:
+          '710418825047-olbh8tqc7vb3fkdv8bkso7sdu3npkvpe.apps.googleusercontent.com',
+      });
+
+      const userData = await GoogleSignin.signIn();
+      const credential = firebase.auth.GoogleAuthProvider.credential(
+        userData.idToken,
+        userData.accessToken,
+      );
+      await firebase.auth().signInWithCredential(credential);
+    } catch (e) {
+      console.log('error');
+      console.error(e);
+    }
   }
 }
